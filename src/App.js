a9270ea0da935b45e98cb249a9ded8a5ec3698f9
lee_c <gitleelee@gmail.com>
2024-04-01 18:08:48 -0700

    Added html, styles, dependencies, etc.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './App.css'; // Import your CSS file
+import { topFunction } from './topFunction.js';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <script src="topFunction.js"></script>
+      <header>
+        <img src="./img/basketball-monk.png" alt="logo" />
+        <h1>Monk's Courses on Financial Literacy</h1>
+        <p><em>"Using sports to bridge the gap in financial literacy so you can profit"</em></p>
       </header>
+
+      <main>
+        <div className="column-icons">
+          <h3>Monk Says...this is how you win</h3>
+          <div className="icons">
+            <img src="./img/banner.png" alt="Time" />
+          </div>
+        </div>
+        <h3>Monk says... this is how you play the game</h3>
+        <div className="column">
+          <p>Why should you invest?</p>
+          <p>Taking a loss</p>
+          <p>Taxes</p>
+          <p>You assign a coach</p>
+          <p>Scouting for players</p>
+          <p>The warmup</p>
+          <p>Practice session</p>
+          <p>Building your own arena</p>
+          <p>Understanding your 401(k)/Pension</p>
+          <p>Generational wealth</p>
+        </div>
+      </main>
+
+      <div className="quote">
+        <strong>Monk Says...</strong>
+        <br />
+        The Game is out there. It's either play or get played.
+        <cite title="Source Title"><br />- Omar, The Wire</cite>
+      </div>
+
+      <footer>
+        <div className="click-btns">
+          <a href="https://docs.google.com/forms/d/e/1FAIpQLSfBiPrSYs_lAhMlIkbXXh7FmVMlKMjzijRtU29tv6Cx3FnZ5A/viewform" target="_blank" rel="noopener noreferrer" className="survey-button">
+            <strong>Q&A</strong>
+          </a>
+          <a href="mailto:whatmonksays@gmail.com" rel="noopener noreferrer" target="_blank" className="email-btn">
+            <img src="img/email-btn.png" alt="Email Button" />
+          </a>
+        </div>
+        <div className="top-button">
+          <button onClick={topFunction} id="topBtn" title="Go to top">Top</button>
+        </div>
+        <p className="copyright">
+          Copyright {new Date().getFullYear()} | Monksays
+        </p>
+      </footer>
     </div>
   );
 }
